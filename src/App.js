3b0ce3a2c9d72f481ffe8f5f0238f7a301a16956
leander006 <leanderdsilva06@gmail.com>
2022-11-01 21:08:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,105 @@
-import React,{useState} from 'react'
-import  News  from './component/News/News';
-import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
-import LoadingBar from 'react-top-loading-bar'
-import Navbar from './component/Navbar/Navbar';
+import React, { useState } from "react";
+import News from "./component/News/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-const App = ()=>{
+const App = () => {
+  const [progress, setProgress] = useState(0);
 
-  const [progress, setProgress] = useState(0)
-
-    return (
-
-      <div>
-    
+  return (
+    <div>
       <Router>
-      <LoadingBar
-        color='#f11946'
-        progress={progress}
-      />
-     <Routes>
-       <Route exact  path="/" element={< News setProgress={setProgress} key="General"   category="General" country="in"  pageSize = "9" />} ></Route>
-       <Route  path="/sports" element={< News  setProgress={setProgress}     key="sport" category="sport" country="in"  pageSize = "9" />} ></Route>
-       <Route path="/business" element={< News setProgress={setProgress}  key="business"   category="business" country="in"  pageSize = "9" />} ></Route>
-       <Route  path="/entertainment" element={< News setProgress={setProgress}  key="entertainment"   category="entertainment" country="in"  pageSize = "9" />} ></Route>
-       <Route  path="/health" element={< News  setProgress={setProgress}   key="health" category="health" country="in"  pageSize = "9" />} ></Route>
-       <Route  path="/science" element={< News setProgress={setProgress}  key="science"   category="science" country="in"  pageSize = "9" />} ></Route>
-       <Route  path="/technology" element={< News setProgress={setProgress}  key="technology"   category="technology" country="in"  pageSize = "9" />} ></Route>
-       </Routes>
-       </Router>
-        
-      </div>
-      
-    )
+        <LoadingBar color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="General"
+                category="General"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sport"
+                category="sport"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                category="business"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                category="entertainment"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                category="health"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                category="science"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+          <Route
+            path="/technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology"
+                category="technology"
+                country="in"
+                pageSize="9"
+              />
+            }
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-}
-
-export default App 
+export default App;
