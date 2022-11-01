c43601a403e474739c8e102e48d01b6b2606bba1
leander006 <leanderdsilva06@gmail.com>
2022-11-01 21:11:55 +0530

    done

--- src/component/News/News.js
+++ src/component/News/News.js
@@ -1,73 +1,82 @@
-import React, {useEffect, useState} from 'react'
-import './news.css';
-import NewsItem from '../NewsItems/NewsItem'
+import React, { useEffect, useState } from "react";
+import "./news.css";
+import NewsItem from "../NewsItems/NewsItem";
 
-import Spinner from '../Spinner/Spinner';
+import Spinner from "../Spinner/Spinner";
 
 import InfiniteScroll from "react-infinite-scroll-component";
-import Navbar from '../Navbar/Navbar';
+import Navbar from "../Navbar/Navbar";
 
-const News = ({category,country,pageSize,setProgress})=>{
-    const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
-  
+const News = ({ category, country, pageSize, setProgress }) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-
-useEffect(() => {
-    const News = async ()=> {
-      
-const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&pageSize=${pageSize}`; 
-        setProgress(10);
-        let data = await fetch(url);
-        setProgress(30);
-        let parsedData = await data.json()
-        setLoading(false);
-        setProgress(70);
-        setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResults)
-        setLoading(false)
-        setProgress(100);
-    }
+  useEffect(() => {
+    const News = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page}&pageSize=${pageSize}`;
+      setProgress(10);
+      let data = await fetch(url);
+      setProgress(30);
+      let parsedData = await data.json();
+      setLoading(false);
+      setProgress(70);
+      setArticles(parsedData.articles);
+      setTotalResults(parsedData.totalResults);
+      setLoading(false);
+      setProgress(100);
+    };
 
     News();
-}, [])
+  }, [loading]);
 
-const fetchMoreData = async () => {   
-    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${page+1}&pageSize=${pageSize}`;
-    setPage(page+1) 
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${
+      process.env.REACT_APP_NEWS_API
+    }&page=${page + 1}&pageSize=${pageSize}`;
+    setPage(page + 1);
     let data = await fetch(url);
-    let parsedData = await data.json()
-    setArticles(articles.concat(parsedData.articles))
-    setTotalResults(parsedData.totalResults)
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
-        return (
-            <>
-            <Navbar/>
-            {!loading ?  <div className='main'>
-           
-                    <h3>Today's News -{category}</h3>
-                 <div className="container">
-                 <InfiniteScroll
-          dataLength={articles?.length}
-          next={fetchMoreData}
-          hasMore={articles?.length !== totalResults}
-          loader={<Spinner/>}>
-              <div className='container'>
-                         <div className="row">
-                            {articles?.map((m) =>(
-                                <NewsItem key={m._id} title={m?.title} desc={m?.description} img={m?.urlToImage} more={m?.url} date={m?.publishedAt} author={m?.author}/>
-                             ))} 
-                        </div>
+  return (
+    <>
+      <Navbar />
+      {!loading ? (
+        <div className="main">
+          <h3>Today's News -{category}</h3>
+          <div className="container">
+            <InfiniteScroll
+              dataLength={articles?.length}
+              next={fetchMoreData}
+              hasMore={articles?.length !== totalResults}
+              loader={<Spinner />}
+            >
+              <div className="container">
+                <div className="row">
+                  {articles?.map((m) => (
+                    <NewsItem
+                      key={m._id}
+                      title={m?.title}
+                      desc={m?.description}
+                      img={m?.urlToImage}
+                      more={m?.url}
+                      date={m?.publishedAt}
+                      author={m?.author}
+                    />
+                  ))}
                 </div>
-                        </InfiniteScroll>
-                    </div> 
-   
-            </div> :<Spinner/>}
-            </>
-        )
-    
-}
+              </div>
+            </InfiniteScroll>
+          </div>
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </>
+  );
+};
 
-export default News
+export default News;
--- src/component/NewsItems/NewsItem.js
+++ src/component/NewsItems/NewsItem.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import './newItem.css'
-const NewsItem = ({title,desc,img,more,date,author})=>{
-        return (
-    <div className='col-sm-6 col-lg-4  my-4'>
-    <div className="card" >
-    <h5 className='author'>Author : {author?author:"Unknown"}</h5>
-      <img src={img?img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkJCXsC9Y4cMn-oPZ85XO1BsoRT9F_jyGHyQ&usqp=CAU"} className="card-img-top" alt=" "/>
-      <div className="card-body">
-        <h5 className="card-title">{title?.length>200?title.substring(0,200).concat("..."):title}</h5>
-        <p className="card-text">{desc?.length>700?desc.substring(0,700).concat("..."):desc}</p>
-        <h6 className='card-text1'>Published on :{new Date(date).toDateString()}</h6>
-        <a href={more} target="_blank"  className="btn1">Read More</a>
+import React from "react";
+import "./newItem.css";
+const NewsItem = ({ title, desc, img, more, date, author }) => {
+  return (
+    <div className="col-sm-6 col-lg-4  my-4">
+      <div className="card">
+        <h5 className="author">Author : {author ? author : "Unknown"}</h5>
+        <img
+          src={
+            img
+              ? img
+              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkJCXsC9Y4cMn-oPZ85XO1BsoRT9F_jyGHyQ&usqp=CAU"
+          }
+          className="card-img-top"
+          alt=" "
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {title?.length > 200
+              ? title.substring(0, 200).concat("...")
+              : title}
+          </h5>
+          <p className="card-text">
+            {desc?.length > 700 ? desc.substring(0, 700).concat("...") : desc}
+          </p>
+          <h6 className="card-text1">
+            Published on :{new Date(date).toDateString()}
+          </h6>
+          <a href={more} target="_blank" rel="noreferrer" className="btn1">
+            Read More
+          </a>
+        </div>
       </div>
-    
     </div>
-    </div> 
+  );
+};
 
-        )
-    
-}
-
-export default  NewsItem 
+export default NewsItem;
